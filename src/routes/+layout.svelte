2ad27dd525eee3c3b5649a1ac6eb2df1f4910942
LeFFaQ <lffqyx@yandex.ru>
2023-10-07 13:46:04 +0700

    fix: {base} path

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,9 @@
 <script lang="ts">
+	import { base } from '$app/paths'
 </script>
 
 <header>
-	<a href="/">Главная</a>
+	<a href={base}>Главная</a>
 
 	<a href="https://kuzstu.ru/web-content/sitecontent/studentu/raspisanie/raspisan.html"
 		>Расписание</a
