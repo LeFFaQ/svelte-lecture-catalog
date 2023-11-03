360ce782b8ede29a3b0c531e29a4c3a3ad76c0a3
LeFFaQ <lffqyx@yandex.ru>
2023-11-03 22:37:28 +0700

    fix: main page date format

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -15,7 +15,9 @@
 	{#if data.posts.length != 0}
 		{#each data.posts as post}
 			<li>
-				<a href="{base}/{post.slug}">{post.title}. {post.author}, {new Date(post.date).toLocaleDateString}</a>
+				<a href="{base}/{post.slug}"
+					>{post.title}. {post.author}, {new Date(post.date).toLocaleDateString()}</a
+				>
 			</li>
 		{/each}
 	{:else}
